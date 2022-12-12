af82239609ff3494b77f3cac324da16e0506120e
Alexey <besh1985@mail.ru>
2022-12-12 13:22:46 +0300

    complite

---
+++ Task21/Program.cs
@@ -0,0 +1,29 @@
+﻿Console.Clear();
+
+Console.Write("Введите координату X1: ");
+double x = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Введите координату Y1: ");
+double y = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Введите координату Z1: ");
+double z = Convert.ToDouble(Console.ReadLine());
+
+
+Console.Write("Введите координату X2: ");
+double x2 = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Введите координату Y2: ");
+double y2 = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Введите координату Z2: ");
+double z2 = Convert.ToDouble(Console.ReadLine());
+
+
+
+double result = Math.Sqrt((x-x2)*(x-x2) + (y-y2)*(y-y2) + (z-z2)*(z-z2));
+
+result = Math.Round(result, 2);
+
+Console.Write(result);
+
---
+++ Task23/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Clear();
+Console.Write("Введите любое число: ");
+
+double n = Convert.ToInt32(Console.ReadLine());
+
+for (int i = 1; i <= n; i++)
+
+Console.WriteLine(Math.Pow(i,3));
+
