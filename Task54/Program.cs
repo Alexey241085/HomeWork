f5fde76daf095249dba922c30b2626024319994c
Alexey <besh1985@mail.ru>
2022-12-29 21:37:03 +0300

    Task 54,56,58, 60 complite

---
+++ Task54/Program.cs
@@ -0,0 +1,53 @@
+﻿
+void InputArray (int[,] array)
+{
+     for (int i = 0; i < array.GetLength(0); i++ )
+   {
+      for (int j = 0; j < array.GetLength(1); j++)
+         array [i,j] = new Random().Next(1,11);
+   }
+} 
+
+void PrintArray (int [,] array)
+{
+   for (int i = 0; i < array.GetLength(0); i++)
+    {
+       for (int j = 0; j < array.GetLength(1); j++)
+       Console.Write($"{array[i,j]} \t");
+       Console.WriteLine();
+
+    }
+
+}
+
+void ReleaseArray (int[,] array)
+{
+  for (int i = 0; i < array.GetLength(0); i++)
+  {  for (int j = 0; j < array.GetLength(1); j++)
+     {
+        for (int k = 0; k < array.GetLength(1) -1; k++)
+        {
+           if (array[i,k] > array[i, k+1])
+           {
+            int temp = array[i, k +1];
+            array[i, k+1] = array[i,k];
+            array[i,k] = temp;
+           }
+        }
+     } 
+  } 
+}
+
+
+Console.Clear();
+Console.Write("Ввести размер массива: ");
+int[] size = Console.ReadLine().Split(" ").Select(x => int.Parse(x)).ToArray(); 
+int[,] array = new int[size[0], size[1]]; 
+InputArray(array);
+Console.WriteLine("Начальный двумерный массив: ");
+PrintArray(array);
+Console.WriteLine();
+ReleaseArray(array);
+Console.WriteLine("Конечный двумерный массив: ");
+PrintArray(array);
+
---
+++ Task56/Program.cs
@@ -0,0 +1,59 @@
+﻿void InputArray (int[,] array)
+{
+     for (int i = 0; i < array.GetLength(0); i++ )
+   {
+      for (int j = 0; j < array.GetLength(1); j++)
+        array [i,j] = new Random().Next(1,11);
+   }
+} 
+
+void PrintArray (int [,] array)
+{
+   for (int i = 0; i < array.GetLength(0); i++)
+    {
+       for (int j = 0; j < array.GetLength(1); j++)
+       Console.Write($"{array[i,j]} \t");
+       Console.WriteLine();
+    }
+}
+
+void NumberRow(int[,] array)
+{
+    int minRow = 0;
+    int minSumRow = 0;
+    int sumRow = 0;
+    for (int i = 0; i < array.GetLength(1); i++)
+    {
+        minRow += array[0, i];
+    }
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++) 
+           sumRow += array[i, j];
+        if (sumRow < minRow)
+        {
+            minRow = sumRow;
+            minSumRow = i;
+        }
+        sumRow = 0;
+    }
+    Console.Write($"{minSumRow + 1} строка");
+}
+
+Console.Clear();
+Console.Write("Введите размер прямоугольного массива: ");
+int[] size = Console.ReadLine().Split(" ").Select(x => int.Parse(x)).ToArray();
+while (size[0] == size[1])
+{
+    Console.Write("Вы ошиблись!\nВведите размер прямоугольного двумерного массива: ");
+    size = Console.ReadLine().Split(" ").Select(x => int.Parse(x)).ToArray();
+}
+int[,] matrix = new int[size[0], size[1]];
+int[,] array = new int[size[0], size[1]];
+InputArray(array);
+Console.WriteLine("Начальный двумерный массив: ");
+PrintArray(array);
+Console.WriteLine();
+NumberRow(array);
+
+
