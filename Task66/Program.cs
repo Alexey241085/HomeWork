1fccb3d80171de3923fae7d6beb622777c2afb7d
Alexey <besh1985@mail.ru>
2023-01-08 20:58:11 +0300

    Task 66, 68 complite

---
+++ Task66/Program.cs
@@ -0,0 +1,24 @@
+﻿
+int SummMN(int a, int b)
+{
+    int result = 0;
+    if (a == b)
+        return 0;
+    else
+    { 
+        a++;
+        result = a + SummMN(a, b);
+        return result;
+    }
+}
+
+Console.Clear();
+Console.Write("Введите число m: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число n: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+SummMN(m, n);
+Console.Write("Сумма чисел от m  до n ровна ");
+Console.Write(SummMN(m - 1, n));
+
